2ff1a129b9693ee00916bbc6983493d165ecf85f
mpacz <maciejpaczocha@gmail.com>
2018-10-21 22:57:37 +0200

    2.04

--- server.js
+++ server.js
@@ -75,9 +75,9 @@ setInterval(function() {
 
 function calcMissiles(){
   for (var id in missiles){
-    if(Math.sqrt(Math.pow(missile.x, 2)) > 2000  || Math.sqrt(Math.pow(missile.y, 2)) > 2000 ){
-      delete missiles[id];
-    }
+    // if(Math.sqrt(Math.pow(missile.x, 2)) > 2000  || Math.sqrt(Math.pow(missile.y, 2)) > 2000 ){
+    //   delete missiles[id];
+    // }
     var missile = missiles[id];
     missile.x = missile.x + missile.vec.x * 25;
     missile.y = missile.y + missile.vec.y * 25;
