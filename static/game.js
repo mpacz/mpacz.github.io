a0c792d1fc5737cd7b124536ef1a8222b488949b
mpacz <maciejpaczocha@gmail.com>
2018-10-21 21:53:06 +0200

    1.44

--- static/game.js
+++ static/game.js
@@ -1,6 +1,6 @@
-var origin = window.location.origin;
-var socket = io.connect(origin);
-//var socket = io();
+//var origin = window.location.origin;
+//var socket = io.connect(origin);
+var socket = io();
 
 var movement = {
     up: false,
