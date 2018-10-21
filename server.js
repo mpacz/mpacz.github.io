7f4a2e3d696e94a0a1c2ec5d45a410f389a8eba7
mpacz <maciejpaczocha@gmail.com>
2018-10-21 15:35:10 +0200

    1.16

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ var express = require('express');
 var Victor = require('victor');
 var http = require('http');
 var path = require('path');
+//var socketIO = require('socket.io');
 var socketIO = require('socket.io');
-//var socketIO = require('socket.io')(http);
 
 var app = express();
 var server = http.Server(app);
